d5f7860851493e8687d7d519c81646ec7df37f2d
Min <mtan121@aucklanduni.ac.nz>
2021-05-04 20:53:03 +0800

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+cats.push(name)
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function removeLastCat() {
+    const copyCats = [... cats]
+    copyCats.pop()
+    return copyCats
+}
+
+function destructivelyRemoveFirstCat() {
+cats.shift()
+}
+
+function removeFirstCat () {
+const copyCats = [... cats]
+copyCats.shift()
+return copyCats
+}
